b2859fd890bd60ecb3d23874de25de8971a31c08
John Burger <John.Adriaan@BigPond.com>
2016-06-18 21:06:44 +1000

    And now that we can have multiple Timers...

--- test/test.ino
+++ test/test.ino
@@ -2,18 +2,18 @@
 
 
 Blink first_blink (13, 500);
-//Blink second_blink (12, 1000);
+Blink second_blink (12, 1000);
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println("Starting...");
   first_blink.begin();
-  //second_blink.begin();
+  second_blink.begin();
 } 
 
 void loop()
 {
     first_blink.updateTimer();
-    //second_blink.updateTimer();
+    second_blink.updateTimer();
 }
